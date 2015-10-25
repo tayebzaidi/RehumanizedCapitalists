99e2e62a10840fc22a1aa66f3eb69c873e9d98c4
Valenti <=>
2015-10-25 01:26:36 -0500

    Initial App Commit

---
+++ EatEasy/app/src/main/java/com/example/valenti/eateasy/LandingScreen.java
@@ -0,0 +1,20 @@
+package com.example.valenti.eateasy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LandingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_screen);
+    }
+
+    public void goToScreen(View view){
+        Intent intent = new Intent(this, Restrictions.class);
+        startActivity(intent);
+    }
+}
