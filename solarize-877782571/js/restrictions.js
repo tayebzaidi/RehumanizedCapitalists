02ed671d1d89a7fc102856ce5cab2881739b4205
Valenti <=>
2015-10-25 11:54:00 -0500

    Completed Restrictions page. Moving on to Ingredients.

---
+++ solarize-877782571/js/restrictions.js
@@ -0,0 +1,18 @@
+var restrictions = []
+
+
+$(document).ready(function(){
+	$('.check-circle').click(function(){
+		if($(this).hasClass('active-check-circle')){
+			$(this).removeClass('active-check-circle');
+			var index = restrictions.indexOf($(this).attr('id'));
+			if(index > -1){
+				restrictions.splice(index, 1);
+			}
+		}
+		else{
+			$(this).addClass('active-check-circle');
+			restrictions.push($(this).attr('id'));
+		}
+	});
+});
